294db8a812b1a7bae22310789eae032956df758b
Sebastian <sbernal@unsa.edu.pe>
2025-12-17 20:59:14 -0500

    Ejercicio  4

--- ejercicio4.py
+++ ejercicio4.py
@@ -15,25 +15,49 @@ class Libro:
         self.disponible=True
 
 class LibroDigital(Libro):
-    def __init__(self,titulo, autor, año, disponible, formato, tamañoMB):
-        super(titulo, autor, año, disponible)
-        self.formato=formato
-        self.tamañoMB=tamañoMB
+    def __init__(self, titulo, autor, año, disponible, formato, tamañoMB):
+        super().__init__(titulo, autor, año, disponible)
+        self.formato = formato
+        self.tamañoMB = tamañoMB
     def prestar(self):
-        print("Libro prestado")
+        print("Libro descargado con exito")
 
 class Biblioteca:
     def __init__(self):
         self.listaLibros=[]
-    def agregar_libro(self, Libro):
-        self.listaLibros.append(Libro)
+    def agregar_libro(self, libro):
+        self.listaLibros.append(libro)
     def listar_libros(self):
-        lista=[]
         for libro in self.listaLibros:
-            lista.append(f"{libro.titulo}, {libro.disponible}")
-        return lista
+            print(f"{libro.titulo}: {libro.disponible}")
     def buscar_por_autor(self, autor):
         for libro in self.listaLibros:
-            if libro.autor==autor:
-                return libro
-        return None
+            if libro.autor.lower()==autor.lower():
+                print(f"{libro.autor}: {libro.titulo}")
+    def prestar_libro(self, titulo):
+        for libro in self.listaLibros:
+            if libro.titulo.lower() == titulo.lower():
+                libro.prestar()
+
+biblioteca=Biblioteca()
+libro1 = Libro("Libro1", "Autor1", 1967, True)
+libro2 = Libro("Libro2", "Autor2", 1605, True)
+libro3 = Libro("Libro3", "Autor3", 1949, True)
+libroD1 = LibroDigital("Libro4", "Autor4", 2020, True, "PDF", 5.2)
+libroD2 = LibroDigital("Libro5", "Autor5", 2008, True, "EPUB", 3.8)
+biblioteca.agregar_libro(libro1)
+biblioteca.agregar_libro(libro2)
+biblioteca.agregar_libro(libro3)
+biblioteca.agregar_libro(libroD1)
+biblioteca.agregar_libro(libroD2)
+
+biblioteca.listar_libros()
+
+biblioteca.prestar_libro("Libro3")
+
+for i in range(5):
+    biblioteca.prestar_libro("Libro4")
+
+biblioteca.prestar_libro("Libro3")
+
+biblioteca.buscar_por_autor("Autor3")
