17d2d813083ca9dff3700adf887e0c95683fa79e
Sebastian <sbernal@unsa.edu.pe>
2025-12-17 22:03:00 -0500

    Ejercicio7 binario

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,13 +1,6 @@
-def copiar_archivo_texto(origen, destino):
-    with open(origen, 'r') as archivo_origen:
+def copiar_archivo_binario(origen, destino):
+    with open(origen, 'rb') as archivo_origen:
         contenido = archivo_origen.read()
-    with open(destino, 'w') as archivo_destino:
+    with open(destino, 'wb') as archivo_destino:
         archivo_destino.write(contenido)
-    print("Archivo de texto copiado")
-
-with open("original.txt", "w") as f:
-    f.write("Hola mundo")
-copiar_archivo_texto("original.txt", "copia.txt")
-print("\nContenido del archivo copiado:")
-with open("copia.txt", "r") as f:
-    print(f.read())
+    print("Archivo binario copiado exitosamente")
